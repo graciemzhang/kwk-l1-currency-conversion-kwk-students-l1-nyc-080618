f3da83d28b1aeb4631d3ba9476cba855f7f10676
graciemzhang <gracie.zhang@lps-students.org>
2018-08-08 19:07:50 +0000

    automatically backed up by learn

--- currency.rb
+++ currency.rb
@@ -1,1 +1,15 @@
-# Write your cod here
+# Write your code here
+
+def usd_to_eur(dollar)
+  euro = dollar * 0.83
+  return euro
+end
+
+def eur_to_usd(euro)
+  dollar = euro * 1.16
+  return dollar
+end
+  
+usd_to_eur(10)
+
+  
